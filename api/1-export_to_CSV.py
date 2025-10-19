78b5db623c3a0f6607dec9e506111c3093314800
KamiPacifique <p.kami@alustudent.com>
2025-10-19 20:52:45 +0000

    add_1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""Module"""
+"""Exports employee TODO list to CSV format"""
 
+import csv
 import requests
 import sys
 
 if __name__ == '__main__':
     employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(employee_id)
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-    user_info = requests.request('GET', user_url).json()
-    todos_info = requests.request('GET', todos_url).json()
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
 
-    employee_name = user_info["name"]
-    employee_username = user_info["username"]
-    task_completed = list(filter(lambda obj:
-                                 (obj["completed"] is True), todos_info))
-    number_of_done_tasks = len(task_completed)
-    total_number_of_tasks = len(todos_info)
+    user_info = user_response.json()
+    todos_info = todos_response.json()
 
-    with open(str(employee_id) + '.csv', "w") as file:
-        [file.write('"' + str(employee_id) + '",' +
-                    '"' + employee_username + '",' +
-                    '"' + str(task["completed"]) + '",' +
-                    '"' + task["title"] + '",' + "\n")
-         for task in todos_info]
+    username = user_info.get("username")
+
+    csv_file = f"{employee_id}.csv"
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todos_info:
+            writer.writerow([
+                employee_id,
+                username,
+                task.get("completed"),
+                task.get("title")
+            ])
